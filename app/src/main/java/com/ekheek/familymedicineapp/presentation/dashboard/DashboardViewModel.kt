15dfd4b56de919b55edebe67f32927595dd2b874
kocerenes <eneskocerr41@gmail.com>
2022-10-25 12:33:15 +0300

    Initial commit

---
+++ app/src/main/java/com/ekheek/familymedicineapp/presentation/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.ekheek.familymedicineapp.presentation.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
