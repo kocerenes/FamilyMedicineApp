c032b4d8831d5c73fddf72c136afeda231e75799
emirk <emir.karabey1@hotmail.com>
2022-10-28 18:03:27 +0300

    Created login screen

---
+++ app/src/main/java/com/ekheek/familymedicineapp/presentation/login/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.ekheek.familymedicineapp.presentation.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ekheek.familymedicineapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
