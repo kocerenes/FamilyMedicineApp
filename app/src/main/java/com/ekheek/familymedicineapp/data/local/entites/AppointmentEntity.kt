45e9f42a08bf80a610d0bcf06207e24d103ecbac
herdal06 <h.erdal721@gmail.com>
2022-10-29 16:48:35 +0300

    Create appointment entity and uimodel

---
+++ app/src/main/java/com/ekheek/familymedicineapp/data/local/entites/AppointmentEntity.kt
@@ -0,0 +1,13 @@
+package com.ekheek.familymedicineapp.data.local.entites
+
+import androidx.room.Entity
+
+@Entity(tableName = "appointments")
+data class AppointmentEntity(
+    val id: String,
+    val patientId: Int,
+    val doctorName: String,
+    val date: String,
+    val time: String,
+    val patientComplaints: String // hasta şikayeti
+)
