6fa17600e5e0235bee77d88f680e4a83da35d5b9
herdal06 <h.erdal721@gmail.com>
2022-10-29 13:58:22 +0300

    Update patient model

--- app/src/main/java/com/ekheek/familymedicineapp/data/model/Patient.kt
+++ app/src/main/java/com/ekheek/familymedicineapp/data/model/Patient.kt
@@ -1,12 +1,12 @@
 package com.ekheek.familymedicineapp.data.model
 
 data class Patient(
-    val id: Int?,
+    val id: String?,
     val mail: String?,
-    val password: String?,
     val name: String?,
     val phone: String?,
     val weight: Double?,
     val height: Double?,
+    val dateOfBirth: String?,
     val age: Int?
 )
