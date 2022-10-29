6dea3904cb98f7d5db2b473e5ee8dd84d675fac8
herdal06 <h.erdal721@gmail.com>
2022-10-29 12:55:09 +0300

    Add patient model

---
+++ app/src/main/java/com/ekheek/familymedicineapp/data/model/Patient.kt
@@ -0,0 +1,12 @@
+package com.ekheek.familymedicineapp.data.model
+
+data class Patient(
+    val id: Int?,
+    val mail: String?,
+    val password: String?,
+    val name: String?,
+    val phone: String?,
+    val weight: Double?,
+    val height: Double?,
+    val age: Int?
+)
