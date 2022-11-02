96f6dacb7b781fa374a833121901e30fc405673b
herdal06 <h.erdal721@gmail.com>
2022-11-02 12:23:45 +0300

    Update method parametres

--- app/src/main/java/com/ekheek/familymedicineapp/presentation/register/ContinueRegisterViewModel.kt
+++ app/src/main/java/com/ekheek/familymedicineapp/presentation/register/ContinueRegisterViewModel.kt
@@ -32,24 +32,37 @@ class ContinueRegisterViewModel(
         return true
     }
 
-    private fun getNewPatientEntry(firstName: String,
-                                   lastName: String,
-                                   phoneNumber: String,
-                                   weight: Double,
-                                   height: Double,
-                                   age: Int): PatientEntity  =
+    private fun getNewPatientEntry(
+        id: String,
+        firstName: String,
+        lastName: String,
+        phoneNumber: String,
+        weight: String,
+        height: String,
+        age: String
+    ): PatientEntity =
         PatientEntity(
+            id = id,
             firstName = firstName,
             lastName = lastName,
             phoneNumber = phoneNumber,
-            weight = weight,
-            height = weight,
-            age = age
+            weight = weight.toDouble(),
+            height = height.toDouble(),
+            age = age.toInt()
         )
 
 
-    fun addNewPatient(itemName: String, itemPrice: String, quantity: String) {
-        val newItem = getNewItemEntry(itemName, itemPrice, quantity)
-        insertItem(newItem)
+    fun addNewPatient(
+        id: String,
+        firstName: String,
+        lastName: String,
+        phoneNumber: String,
+        weight: String,
+        height: String,
+        age: String
+    ) {
+        val newItem =
+            getNewPatientEntry(id = id, firstName, lastName, phoneNumber, weight, height, age)
+        insertPatient(newItem)
     }
 }
