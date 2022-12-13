c1d83087daaa4d436be702b0fa88b7a7a5d12fd3
kocerenes <eneskocerr41@gmail.com>
2022-12-13 21:30:58 +0300

    Add the google-services file

--- app/src/main/java/com/ekheek/familymedicineapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/ekheek/familymedicineapp/data/local/AppDatabase.kt
@@ -3,7 +3,6 @@ package com.ekheek.familymedicineapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.ekheek.familymedicineapp.data.local.dao.AppointmentDao
 import com.ekheek.familymedicineapp.data.local.dao.PatientDao
 import com.ekheek.familymedicineapp.data.local.entities.AvailableAppointmentEntity
 import com.ekheek.familymedicineapp.data.local.entities.PatientEntity
@@ -21,5 +20,4 @@ import com.ekheek.familymedicineapp.data.local.entities.ReceivedAppointmentEntit
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun patientDao(): PatientDao
-    abstract fun appointmentDao(): AppointmentDao
 }
